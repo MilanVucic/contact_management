b334911a2ffc2afbadfabcd5698faf2ddf96edea
Milan Vucic <milan.vucic@labs.devana.rs>
2016-06-30 16:31:01 +0200

    A bit of styling added

--- resources/views/contact/allContacts.blade.php
+++ resources/views/contact/allContacts.blade.php
@@ -13,41 +13,39 @@
                 There are currently no contacts in the list.
             </div>
         @endif
-            <div>
-                <table id="myTable" class="tablesorter" border="1">
-                    <thead>
-                        <tr>
-                            <th>Name <span></span></th>
-                            <th>Nickname</th>
-                            <th>Date Met</th>
-                            <th>Date Added</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($contacts as $contact)
-                        <tr>
-                            <td><a href="contact/{{$contact->getId()}}">{{$contact->getName()}}</a></td>
-                            <td>{{$contact->getNickname()}}</td>
-                            <td>{{$contact->getDateMet()}}</td>
-                            <td>{{$contact->getCreatedAt()}}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
+        <div>
+            <table id="myTable" class="tablesorter table table-striped">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Nickname</th>
+                    <th>Date Met</th>
+                    <th>Date Added</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($contacts as $contact)
+                    <tr>
+                        <td><a href="contact/{{$contact->getId()}}">{{$contact->getName()}}</a></td>
+                        <td>{{$contact->getNickname()}}</td>
+                        <td>{{$contact->getDateMet()}}</td>
+                        <td>{{$contact->getCreatedAt()}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 
     </div>
 @endsection
-<link href="/js/tablesorter/themes/blue/style.css" rel="stylesheet" type="text/css" />
+<link href="/js/tablesorter/themes/blue/style.css" rel="stylesheet" type="text/css"/>
 
-    <script type="text/javascript" src="/js/jquery-3.0.0.js"></script>
-    <script type="text/javascript" src="/js/tablesorter/jquery-latest.js"></script>
-    <script type="text/javascript" src="/js/tablesorter/jquery.tablesorter.min.js"></script>
+<script type="text/javascript" src="/js/tablesorter/jquery-latest.js"></script>
+<script type="text/javascript" src="/js/tablesorter/jquery.tablesorter.min.js"></script>
 
-    <script>
-        $(document).ready(function($)
-                {
-                    $("#myTable").tablesorter();
-                }
-        );
-    </script>
+<script>
+    $(document).ready(function ($) {
+                $("#myTable").tablesorter();
+            }
+    );
+</script>
